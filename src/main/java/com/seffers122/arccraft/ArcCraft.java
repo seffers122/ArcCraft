c2e96156bdb7b05415920bca4bcbdf7e9d86ebb0
seffers122 <eseth51@yahoo.com>
2014-10-30 16:05:48 -0600

    Addition of all basic files and start of proxies

---
+++ src/main/java/com/seffers122/arccraft/ArcCraft.java
@@ -0,0 +1,31 @@
+package com.seffers122.arccraft;
+
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="ArcCraft", name="ArcCraft", version = "1.0")
+public class ArcCraft
+{
+    @Mod.Instance("ArcCraft")
+    public static ArcCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
+
---
+++ src/main/java/com/seffers122/arccraft/proxy/CommonProxy.java
@@ -0,0 +1,7 @@
+package com.seffers122.arccraft.proxy;
+
+/**
+ * Created by seffe_000 on 10/30/2014.
+ */
+public class CommonProxy {
+}
